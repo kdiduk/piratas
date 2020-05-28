5712c821d648bba6f625d6e0056f0b4c729844d8
Kirill Diduk <kdiduk@tuta.io>
2020-05-28 23:08:09 +0200

    Scale tiles/sprites 4 times. Implement mouse clicks

--- godot/level/Level.gd
+++ godot/level/Level.gd
@@ -5,12 +5,15 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
+onready var player = $TileMap/Galeon
+onready var map = $TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func _input(event: InputEvent):
+	if event is InputEventMouseButton and event.is_pressed():
+		var next = map.world_to_map(event.position)
+		player.position = map.map_to_world(next)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
