ed4f7c2c337e690731432e9b89c0a9d5e996c161
Kirill Diduk <kdiduk@tuta.io>
2020-06-01 11:53:42 +0200

    Add camera. Create HUD (empty yet)

--- godot/level/Level.gd
+++ godot/level/Level.gd
@@ -5,8 +5,9 @@ extends Node2D
 # var a = 2
 # var b = "text"
 
-onready var player = $TileMap/Galeon
+onready var player = $Galeon
 onready var map = $TileMap
+onready var camera = $Camera2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,6 +15,24 @@ func _ready():
 
 func _input(event: InputEvent):
 	if event is InputEventMouseButton and event.is_pressed():
-		var next = map.world_to_map(event.position)
-		player.position = map.map_to_world(next)
+		var next = map.world_to_map(event.position + camera.offset)
+		if map.get_cellv(next) == 1:
+			player.position = map.map_to_world(next)
 
+func _process(delta):
+	var offset = Vector2(0, 0)
+	if Input.is_action_pressed("camera_scroll_left"):
+		offset.x -= 8
+	elif Input.is_action_pressed("camera_scroll_right"):
+		offset.x += 8
+
+	if Input.is_action_pressed("camera_scroll_up"):
+		offset.y -= 8
+	elif Input.is_action_pressed("camera_scroll_down"):
+		offset.y += 8
+
+	offset.x = clamp(camera.offset.x + offset.x,
+			-120, 2048)
+	offset.y = clamp(camera.offset.y + offset.y,
+			-40, 1440)
+	camera.offset = offset
